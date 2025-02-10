b3c5926ee00909b5f5fffc8bd915bdf50d49976c
wcrmie <ImWormTheKing@gmail.com>
2025-02-09 19:12:44 -0500

    Added causes

--- Antispam/EthanPedrick_Chapter2_Exercise2.py
+++ Antispam/EthanPedrick_Chapter2_Exercise2.py
@@ -8,11 +8,11 @@ def main():
 
     # Get user input
     potential_scam = input("Copy (CTRL+C) and Paste (CTRL+V) an email here, then press enter to receive a score for how likely it is to be a scam. ").lower()
-    score = scan(potential_scam)
+    score, reasons = scan(potential_scam)
 
     # Use the score to choose which message to send to the user; if there are more than 4 of the keywords in the email, it is probably a scam.
     if (score > 4):
-        print(f"This email has a scam score of {score}, and is likely a scam. ")
+        print(f"This email has a scam score of {score}, and is likely a scam. Causes: {reasons}")
 
     else:
         print(f"This email has a scam score of {score}, and is likely not a scam, but be careful regardless! ")
@@ -21,12 +21,15 @@ def scan(mail):
 
     # set zero variable
     score = 0
+    occurences = []
 
     # loop through the constant list
     for x in KEYWORDS:
 
         # add the count of any given entry to the score
         score += mail.count(x)
-    return score
+        if mail.count(x) > 0:
+            occurences.append(x)
+    return score, occurences
 
 main()
